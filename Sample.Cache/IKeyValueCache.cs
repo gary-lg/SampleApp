338c8cd2bbd59bd8cbeaf9b70a5f2769ec7d8e0e
Gary H <17553415+gary-lg@users.noreply.github.com>
2022-06-06 10:49:23 +0100

    Add Sample.Cache code

---
+++ Sample.Cache/IKeyValueCache.cs
@@ -0,0 +1,25 @@
+﻿namespace Sample.Cache;
+
+///<summary>Provides a mechanism for storing data using a string based key for a period of time</summary>
+public interface IKeyValueCache
+{
+    ///<summary>
+    /// Retrieve a value from the cache with the specified key. If the item could not be located
+    /// in the cache  or is not of the expected type then we return default(T).
+    /// </summary>
+    /// <typeparam name="T">Type of the object to retrieve</typeparam>
+    /// <returns>Value retrieved from the cache or default(T) if it could not be found</returns>
+    T? Get<T>(string key);
+
+    ///<summary>
+    /// Write a value into the cache. The item will remain until expiry or until it is evicted
+    /// due to cache pressure.
+    /// </summary>
+    void Set<T>(string key, T value, TimeSpan expireIn);
+
+    /// <summary>
+    /// Clear the cache, erasing all currently held values
+    /// </summary>
+    /// <returns>Task to await</returns>
+    void Clear();
+}
---
+++ Sample.Cache/InMemoryCache.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.Caching;
+using Sample.Core;
+
+namespace Sample.Cache;
+
+/// <inheritdoc cref="IKeyValueCache" />
+public class InMemoryCache : IKeyValueCache, IDisposable
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private MemoryCache _cache;
+
+    public InMemoryCache(
+        string name,
+        IDateTimeProvider dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _cache = new MemoryCache(name);
+    }
+
+    /// <inheritdoc />
+    public T? Get<T>(string key)
+    {
+        var val = _cache.Get(key);
+        if (val is T v)
+        {
+            return v;
+        }
+
+        return default;
+    }
+    
+    /// <inheritdoc />
+    public void Set<T>(string key, T value, TimeSpan expireIn)
+    {
+        _cache.Set(key, value, _dateTimeProvider.UtcNow.Add(expireIn));
+    }
+
+    /// <inheritdoc />
+    public void Clear()
+    {
+        var newCache = new MemoryCache(_cache.Name);
+        var oldCache = Interlocked.Exchange(ref _cache, newCache);
+        oldCache.Dispose();
+    }
+
+    public void Dispose()
+    {
+        _cache.Dispose();
+    }
+}
